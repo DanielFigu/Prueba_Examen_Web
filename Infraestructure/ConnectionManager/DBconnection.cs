306ea25ffd615653129cc77a95f0ce685c099c45
DanielFigu <163463666+DanielFigu@users.noreply.github.com>
2024-11-11 14:22:41 -0600

    Agregar archivos de proyecto.

---
+++ Infraestructure/ConnectionManager/DBconnection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure
+{
+    public class DBconnection
+    {
+        private OleDbConnection _connection;
+        private string _stringConnection;
+
+        public DBconnection(string DataSourse, string DataBase, string Provider)
+        {
+            _stringConnection = $"Provider={Provider}; Data Sourse={DataSourse}; Initial Catalog={DataBase}; Integrated Security = SSPI; TrustManagerContext Server Certificate=false";
+        
+        }
+
+        public void CreateConnection() 
+        {
+            _connection = new OleDbConnection(_stringConnection) ;
+        }
+
+        public OleDbConnection Open() 
+        {
+            if (_connection == null) return null ;
+
+            if (_connection.State == ConnectionState.Open) return _connection ;
+            else
+            {
+                _connection.Open();
+                return _connection ;
+            }
+        
+        }
+
+        public void Close() 
+        {
+            _connection.Close();
+        }
+
+
+
+    }
+}
---
+++ Infraestructure/Repository/Base/RepositoryBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Repository.Base
+{
+    public abstract class RepositoryBase
+    {
+        public OleDbConnection _connection;
+
+
+        protected RepositoryBase() 
+        {
+            DBconnection connection = new DBconnection("(local)", "Northwind", "MSOLEDBSQL.1");
+            _connection = connection.Open();
+        
+        }
+
+        public abstract string ProcessRequest();
+    }
+
+
+}
